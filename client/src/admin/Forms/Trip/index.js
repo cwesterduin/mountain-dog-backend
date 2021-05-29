6ff983ad7308018496aa16e2e7b6fa1590176fcc
chris westerduin <chriswesterduin@outlook.com>
2021-05-29 09:37:34 +0000

    Trip form frontend working

--- client/src/admin/Forms/Trip/index.js
+++ client/src/admin/Forms/Trip/index.js
@@ -3,47 +3,95 @@ import { TextField, Button } from "@material-ui/core";
 import * as formStyles from "../form.module.css";
 import { useParams } from "@reach/router";
 import { getData } from "../../requests";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 function Trip() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
     Name: "",
     Description: "",
+    TripMediaID: null,
   });
+  const [imageOptions, setImageOptions] = useState([])
   useEffect(() => {
     async function fetchFormState() {
       const data = await getData(`trips/${id}`);
       setFormData({
         Name: data.Name,
         Description: data.Description,
+        TripMediaID: {MediaID: data.TripMediaID},
       });
+      setLoading(true)
+
     }
+    async function fetchImageOptions() {
+        const data = await getData(`media`);
+        setImageOptions(
+            data.results
+        );
+      }
     id && fetchFormState();
+    fetchImageOptions()
   }, []);
+
+  function handleInputChange(e){
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    setFormData({
+        ...formData,
+      [name]: value
+    });
+  }
+
+  if (loading) {
+
   return (
     <form className={formStyles.form} noValidate autoComplete="off">
       <TextField
         id="standard-basic"
         label="Name"
+        name="Name"
         value={formData.Name}
-        onChange={(e) => setFormData({ ...formData, Name: e.target.input })}
+        onChange={handleInputChange}
       />
       <TextField
         id="standard-basic"
         label="Description"
+        name="Description"
         multiline
         rows={4}
         rowsMax={Infinity}
         value={formData.Description}
-        onChange={(e) =>
-          setFormData({ ...formData, Description: e.target.input })
-        }
+        onChange={handleInputChange}
+      />
+       <Autocomplete
+        id="image-select"
+        value={formData.TripMediaID}
+        renderOption={(option) => (
+            <>
+            <a style={{zIndex:'1000'}} target="_blank" href={`https://alfie192345.s3.eu-west-2.amazonaws.com/images/${option.Path}`}><img src={`https://alfie192345.s3.eu-west-2.amazonaws.com/images/${option.Path}`} height={48} width={48} /></a>
+            <div>{option.Path}</div>
+            </>
+        )}
+        onChange={(e, newValue) =>
+            setFormData({ ...formData, TripMediaID: newValue })
+          }
+
+        options={imageOptions}
+        getOptionLabel={(option) => String(option.MediaID)}
+        renderInput={(params) => (
+          <TextField {...params} label="Trip Image" />
+        )}
       />
       <Button variant="contained" color="primary" type="submit">
         {id ? "Save" : "submit"}
       </Button>
-    </form>
+    </form> 
   );
+         } else { return <div>loading...</div> }
 }
 
 export default Trip;
